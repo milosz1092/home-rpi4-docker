30aa1e97d5228369550372e0bf3bc994682e8cfd
Milosz Szewczyk <milosz1092@gmail.com>
2020-05-31 12:40:35 +0200

    Update submodules

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd home-rpi4-client
-npx npm-force-resolutions
 npm install
 npm run build:prod
 
@@ -13,4 +12,4 @@ cd ..
 docker buildx create --name armbuilder || true
 docker buildx use armbuilder
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/arm/v7 -t mewash/home-rpi4-server --push .
+docker buildx build --platform linux/arm/v7,linux/amd64 -t mewash/home-rpi4-server --push .
